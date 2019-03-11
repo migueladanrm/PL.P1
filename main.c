9ec915556a61f6ecd9acef9a032631b8c519fe6e
Miguel Rivas <migueladanrm@outlook.com>
2019-03-11 03:29:39 -0600

    Números aleatorios Se consiguió de forma parcial generar números aleatorios.

--- main.c
+++ main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 #include "env_tests.h"
 
+time_t now;
+
+int random_test() {
+    srand(time(now));
+
+    for (int i = 0; i < 1; ++i) {
+        double tmp = log2(rand());
+        int dec = (int) ((tmp - (int) tmp) * 100);
+        int pair = dec % 2;
+
+        return pair;
+    }
+}
+
+
+void delay(int number_of_seconds) {
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * sqrt(number_of_seconds) * number_of_seconds;
+
+    // Stroing start time
+    clock_t start_time = clock();
+
+    // looping till required time is not acheived
+    while (clock() < start_time + milli_seconds);
+}
+
 int main() {
-    printf("Hello, dude!");
+    for (int i = 0; i < 10; i++) {
+        printf("%i\n", random_test());
+        delay(1);
+    }
+
     return 0;
 }
