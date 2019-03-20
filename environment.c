2ec2bb05b6069106fa6786277e62a3542e9fd682
Miguel Rivas <migueladanrm@outlook.com>
2019-03-20 10:40:32 -0600

    Entorno - Se ha creado archivo para estructuras y procedimientos de simulación de entorno básico.

---
+++ environment.c
@@ -0,0 +1,12 @@
+//
+// Created by Miguel on 20/03/2019.
+//
+
+#include "environment.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct environment_update{
+    double movement;
+    double temperature;
+};
---
+++ environment.h
@@ -0,0 +1,8 @@
+//
+// Created by Miguel on 20/03/2019.
+//
+
+#ifndef PL_P1_ENVIRONMENT_H
+#define PL_P1_ENVIRONMENT_H
+
+#endif //PL_P1_ENVIRONMENT_H
